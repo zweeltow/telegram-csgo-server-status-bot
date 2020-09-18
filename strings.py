a4e6bd012919dfd6e0186284a73cf457590fd663
zweeltow <glebbygaev@gmail.com>
2020-09-18 20:19:54 +0300

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,40 @@
+"""Text of Commands"""
+# English
+cmdStart_en = "👋🏼 Hey, {}!\nThis bot is designed to check the number of online players and the availability of CS:GO servers.\n\nFor more information type /help."
+cmdHelp_en = "ℹ️ This bot is designed by @csgobeta to check the number of online players and the availability of CS:GO servers.\n\n<b>Here are the available commands:</b>\n/status – сheck the availability of the servers\n/mm – show the count of players currently playing\n/dc – see the status of a specific data center\n/feedback – leave feedback about the bot\n/help – get this message\n\nNote that this bot also works automatically, no need to add it anywhere. Simply open any of your chats and type ‘<code>@csgobetabot </code>‘ in the message field. Then tap on a result to send."
+cmdFeedback_en = "💬 Please tell us about your <b>suggestions</b> or <b>problems</b> that you have encountered using our bot.\n\nUse /cancel to cancel this command.\n\nIf you need a dialogue with the developer, you can PM: @zweel <i>(But I'm bad at English 🙂)</i>."
+# Russian
+cmdStart_ru = "👋🏼 Привет, {}!\nЭтот бот предназначен для проверки количества онлайн игроков и доступности CS:GO серверов.\n\nДля большей информации воспользуйтесь /help."
+cmdHelp_ru = "ℹ️ Этот бот разработан каналом @csgobeta и предназначен для проверки количества онлайн игроков и доступности CS:GO серверов.\n\n<b>Доступные команды:</b>\n/status – проверить доступность серверов\n/mm – показать количество онлайн игроков\n/dc – посмотреть состояние дата-центра (на английском языке)\n/feedback – оставить фидбэк про бота\n/help – получить это сообщение\n\nОбратите внимание, что этот бот также работает автоматически, нет необходимости добавлять его куда-либо. Просто откройте любой из Ваших чатов и введите ‘<code>@csgobetabot </code>ʼ в поле сообщения. Затем нажмите на результат для отправки."
+cmdFeedback_ru = "💬 Пожалуйста, расскажите о Ваших <b>пожеланиях</b> или <b>проблемах</b>, с которыми Вы столкнулись, используя бота.\n\nИспользуйте /cancel, чтобы отменить команду.\n\nЕсли Вы желаете диалога с разработчиком, то возможно общение в личной переписке: @zweel."
+
+
+"""Text of Status"""
+# English
+statusNormal_en = "✅ Server status is normal:\n\n— Player count: {}.\n\nLatest update on {} (UTC−8, summer UTC−7)."
+statusWrong_en = "❌ Server status is not normal.\n\nLatest update on {} (UTC−8, summer UTC−7)."
+# Russian
+statusNormal_ru = "✅ Сервера в нормальном состоянии:\n\n— Количество игроков: {}.\n\nОбновлено {} (UTC−8, летом UTC−7)."
+statusWrong_ru = "❌ Сервера в ненормальном состоянии.\n\nОбновлено {} (UTC−8, летом UTC−7)."
+
+
+"""Text of Matchmaking"""
+# English
+mmNormal_en = "✅ Matchmaking scheduler status is normal:\n\n— Online servers: {};\n— Active players: {};\n— Searching players: {};\n— Average search seconds: {} sec.\n\nLatest update on {} (UTC−8, summer UTC−7)."
+mmWrong_en = "❌ Matchmaking scheduler status is not normal.\n\nLatest update on {} (UTC−8, summer UTC−7)."
+# Russian
+mmNormal_ru = "✅ Планировщик матчмейкинга в нормальном состоянии:\n\n— Онлайн серверов: {};\n— Активных игроков: {};\n— Игроков в поиске: {};\n— Среднее время поиска: {} сек.\n\nОбновлено {} (UTC−8, летом UTC−7)."
+mmWrong_ru = "❌ Планировщик матчмейкинга в ненормальном состоянии.\n\nLatest update on {} (UTC−8, summer UTC−7)."
+
+
+"""Text of Wrong Request"""
+
+unknownRequest_en = "🤷‍♀️ Nothing found, please use one of the following commands:"
+unknownRequest_ru = "🤷‍♀️ Ничего не найдено, пожалуйста, воспользуйтесь одной из приведённых команд:"
+
+wrongAPI_en = "💀 Issues with Valve's API, the bot can't get information, please, try again later."
+wrongAPI_ru = "💀 Проблемы с API Valve, бот не может получить информацию, пожалуйста, подождите несколько минут."
+
+
+"""DC"""
+
