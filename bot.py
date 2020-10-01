b8b7c9217054e82924df5e094c814e5575f09187
zweeltow <glebbygaev@gmail.com>
2020-10-01 09:22:03 +0300

    кодить всё ещё не научился
    совсем

--- bot.py
+++ bot.py
@@ -71,7 +71,7 @@ markup_DC_Back.add(Back_button)
 markup_ru = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
 Status_ru = types.KeyboardButton('Статус')
 Matchmaking_ru = types.KeyboardButton('Матчмейкинг')
-DC_ru = types.KeyboardButton('Дата-центры')
+DC_ru = types.KeyboardButton('Дата-центры (Англ.)')
 markup_ru.add(Status_ru, Matchmaking_ru, DC_ru)
 
 # DC RU
@@ -199,13 +199,13 @@ def dc(message):
 
 def dc_africa(message):
     capacity, load, time_server = api_dc.africa_South()
-    text = f'🌍 South Africa DCʼ status is OK:\n\n— Location: Johannesburg;\n— Load: {load};\n— Capacity: {capacity}.\n\nLatest update on {time_server} (UTC−8, summer UTC−7).'
+    text = f'🌍 South Africa DCʼ status is OK:\n\n• Location: Johannesburg;\n• Load: {load};\n• Capacity: {capacity}.\n\nLatest update on {time_server} (UTC−8, summer UTC−7).'
     bot.send_message(message.chat.id, text)
 
 
 def dc_australia(message):
     capacity, load, time_server = api_dc.australia()
-    text = f'🇦🇺 Australia DCʼ status is OK:\n\n— Location: Sydney;\n— Load: {load};\n— Capacity: {capacity}.\n\nLatest update on {time_server} (UTC−8, summer UTC−7).'
+    text = f'🇦🇺 Australia DCʼ status is OK:\n\n• Location: Sydney;\n• Load: {load};\n• Capacity: {capacity}.\n\nLatest update on {time_server} (UTC−8, summer UTC−7).'
     bot.send_message(message.chat.id, text)
 
 
@@ -216,19 +216,19 @@ def dc_europe(message):
 
 def dc_eu_north(message):
     capacity, load, time_server = api_dc.eu_North()
-    text = f'🇪🇺 North Europe DCʼ status is OK:\n\n— Location: Stockholm;\n— Load: {load};\n— Capacity: {capacity}.\n\nLatest update on {time_server} (UTC−8, summer UTC−7).'
+    text = f'🇪🇺 North Europe DCʼ status is OK:\n\n• Location: Stockholm;\n• Load: {load};\n• Capacity: {capacity}.\n\nLatest update on {time_server} (UTC−8, summer UTC−7).'
     bot.send_message(message.chat.id, text, reply_markup=markup_DC)
 
 
 def dc_eu_west(message):
     capacity, load, capacity_Spain, load_Spain, time_server = api_dc.eu_West()
-    text = f'🇪🇺 West Europe DCʼ status is OK:\n\n— Location: Luxembourg;\n— Load: {load};\n— Capacity: {capacity}.\n\n🇪🇸 Spain DCʼ status is OK:\n\n— Location: Mardid;\n— Load: {load_Spain};\n— Capacity: {capacity_Spain}.\n\nLatest update on {time_server} (UTC−8, summer UTC−7).'
+    text = f'🇪🇺 West Europe DCʼ status is OK:\n\n• Location: Luxembourg;\n• Load: {load};\n• Capacity: {capacity}.\n\n🇪🇸 Spain DCʼ status is OK:\n\n• Location: Mardid;\n• Load: {load_Spain};\n• Capacity: {capacity_Spain}.\n\nLatest update on {time_server} (UTC−8, summer UTC−7).'
     bot.send_message(message.chat.id, text, reply_markup=markup_DC)
 
 
 def dc_eu_east(message):
     capacity_East, capacity_Poland, load_East, load_Poland, time_server = api_dc.eu_East()
-    text = f'🇪🇺 East Europe DCʼ status is OK:\n\n— Location: Vienna;\n— Load: {load_East};\n— Capacity: {capacity_East}.\n\n🇵🇱 Poland DCʼ status is OK:\n\n— Location: Warsaw;\n— Load: {load_Poland};\n— Capacity: {capacity_Poland}.\n\nLatest update on {time_server} (UTC−8, summer UTC−7).'
+    text = f'🇪🇺 East Europe DCʼ status is OK:\n\n• Location: Vienna;\n• Load: {load_East};\n• Capacity: {capacity_East}.\n\n🇵🇱 Poland DCʼ status is OK:\n\n• Location: Warsaw;\n• Load: {load_Poland};\n• Capacity: {capacity_Poland}.\n\nLatest update on {time_server} (UTC−8, summer UTC−7).'
     bot.send_message(message.chat.id, text, reply_markup=markup_DC)
 
 
@@ -244,55 +244,55 @@ def dc_usa(message):
 
 def dc_usa_north(message):
     capacity_US_Northcentral, capacity_US_Northeast, capacity_US_Northwest, load_US_Northcentral, load_US_Northeast, load_US_Northwest, time_server = api_dc.usa_North()
-    text = f'🇺🇸 Northcentral DCʼ status is OK:\n\n— Location: Chicago;\n— Load: {load_US_Northcentral};\n— Capacity: {capacity_US_Northcentral}.\n\n🇺🇸 Northeast DCʼ status is OK:\n\n— Location: Sterling;\n— Load: {load_US_Northeast};\n— Capacity: {capacity_US_Northeast}.\n\n 🇺🇸 Northwest DCʼ status is OK:\n\n— Location: Moses Lake;\n— Load: {load_US_Northwest};\n— Capacity: {capacity_US_Northwest}.\n\nLatest update on {time_server} (UTC−8, summer UTC−7).'
+    text = f'🇺🇸 Northcentral DCʼ status is OK:\n\n• Location: Chicago;\n• Load: {load_US_Northcentral};\n• Capacity: {capacity_US_Northcentral}.\n\n🇺🇸 Northeast DCʼ status is OK:\n\n• Location: Sterling;\n• Load: {load_US_Northeast};\n• Capacity: {capacity_US_Northeast}.\n\n 🇺🇸 Northwest DCʼ status is OK:\n\n• Location: Moses Lake;\n• Load: {load_US_Northwest};\n• Capacity: {capacity_US_Northwest}.\n\nLatest update on {time_server} (UTC−8, summer UTC−7).'
     bot.send_message(message.chat.id, text, reply_markup=markup_DC)
 
 
 def dc_usa_south(message):
     capacity_US_Southeast, capacity_US_Southwest, load_US_Southeast, load_US_Southwest, time_server = api_dc.usa_South()
-    text = f'🇺🇸 Southwest DCʼ status is OK:\n\n— Location: Los Angeles;\n— Load: {load_US_Southwest};\n— Capacity: {capacity_US_Southwest}.\n\n 🇺🇸 Southeast DCʼ status is OK:\n\n— Location: Atlanta;\n— Load: {load_US_Southeast};\n— Capacity: {capacity_US_Southeast}.\n\nLatest update on {time_server} (UTC−8, summer UTC−7).'
+    text = f'🇺🇸 Southwest DCʼ status is OK:\n\n• Location: Los Angeles;\n• Load: {load_US_Southwest};\n• Capacity: {capacity_US_Southwest}.\n\n 🇺🇸 Southeast DCʼ status is OK:\n\n• Location: Atlanta;\n• Load: {load_US_Southeast};\n• Capacity: {capacity_US_Southeast}.\n\nLatest update on {time_server} (UTC−8, summer UTC−7).'
     bot.send_message(message.chat.id, text, reply_markup=markup_DC)
 
 
 def dc_south_america(message):
     capacity_Chile, capacity_Peru, capacity_Brazil, load_Chile, load_Peru, load_Brazil, time_server = api_dc.sa()
-    text = f'🇧🇷 Brazil DCʼ status is OK:\n\n— Location: Sao Paulo;\n— Load: {load_Brazil};\n— Capacity: {capacity_Brazil}.\n\n🇨🇱 Chile DCʼ status is OK:\n\n— Location: Santiago;\n— Load: {load_Chile};\n— Capacity: {capacity_Chile}.\n\n🇵🇪 Peru DCʼ status is OK:\n\n— Location: Lima;\n— Load: {load_Peru};\n— Capacity: {capacity_Peru}.\n\nLatest update on {time_server} (UTC−8, summer UTC−7).'
+    text = f'🇧🇷 Brazil DCʼ status is OK:\n\n• Location: Sao Paulo;\n• Load: {load_Brazil};\n• Capacity: {capacity_Brazil}.\n\n🇨🇱 Chile DCʼ status is OK:\n\n• Location: Santiago;\n• Load: {load_Chile};\n• Capacity: {capacity_Chile}.\n\n🇵🇪 Peru DCʼ status is OK:\n\n• Location: Lima;\n• Load: {load_Peru};\n• Capacity: {capacity_Peru}.\n\nLatest update on {time_server} (UTC−8, summer UTC−7).'
     bot.send_message(message.chat.id, text, reply_markup=markup_DC)
 
 
 def dc_india(message):
     capacity, capacity_East, load, load_East, time_server = api_dc.india()
-    text = f'🇮🇳 India DCʼ status is OK:\n\n— Location: Mumbai;\n— Load: {load};\n— Capacity: {capacity}.\n\n🇮🇳 East India DCʼ status is OK:\n\n— Location: Chennai;\n— Load: {load_East};\n— Capacity: {capacity_East}.\n\nLatest update on {time_server} (UTC−8, summer UTC−7).'
+    text = f'🇮🇳 India DCʼ status is OK:\n\n• Location: Mumbai;\n• Load: {load};\n• Capacity: {capacity}.\n\n🇮🇳 East India DCʼ status is OK:\n\n• Location: Chennai;\n• Load: {load_East};\n• Capacity: {capacity_East}.\n\nLatest update on {time_server} (UTC−8, summer UTC−7).'
     bot.send_message(message.chat.id, text, reply_markup=markup_DC)
 
 
 def dc_japan(message):
     capacity, load, time_server = api_dc.japan()
-    text = f'🇯🇵 Japan DCʼ status is OK:\n\n— Location: Tokyo;\n— Load: {load};\n— Capacity: {capacity}.\n\nLatest update on {time_server} (UTC−8, summer UTC−7).'
+    text = f'🇯🇵 Japan DCʼ status is OK:\n\n• Location: Tokyo;\n• Load: {load};\n• Capacity: {capacity}.\n\nLatest update on {time_server} (UTC−8, summer UTC−7).'
     bot.send_message(message.chat.id, text, reply_markup=markup_DC)
 
 
 def dc_china(message):
     capacity_Shanghai, capacity_Tianjin, capacity_Guangzhou, load_Shanghai, load_Tianjin, load_Guangzhou, time_server = api_dc.china()
-    text = f'🇨🇳 China DCʼ status is OK: \n\n— Location: Shanghai;\n— Load: {load_Shanghai};\n— Capacity: {capacity_Shanghai}.\n\n— Location: Tianjin;\n— Load: {load_Tianjin};\n— Capacity: {capacity_Tianjin}.\n\n— Location: Guangzhou;\n— Load: {load_Guangzhou};\n— Capacity: {capacity_Guangzhou}.\n\nLatest update on {time_server} (UTC−8, summer UTC−7).'
+    text = f'🇨🇳 China DCʼ status is OK: \n\n• Location: Shanghai;\n• Load: {load_Shanghai};\n• Capacity: {capacity_Shanghai}.\n\n• Location: Tianjin;\n• Load: {load_Tianjin};\n• Capacity: {capacity_Tianjin}.\n\n• Location: Guangzhou;\n• Load: {load_Guangzhou};\n• Capacity: {capacity_Guangzhou}.\n\nLatest update on {time_server} (UTC−8, summer UTC−7).'
     bot.send_message(message.chat.id, text, reply_markup=markup_DC)
 
 
 def dc_emirates(message):
     capacity, load, time_server = api_dc.emirates()
-    text = f'🇦🇪 Emirates DCʼ status is OK:\n\n— Location: Dubai;\n— Load: {load};\n— Capacity: {capacity}.\n\nLatest update on {time_server} (UTC−8, summer UTC−7).'
+    text = f'🇦🇪 Emirates DCʼ status is OK:\n\n• Location: Dubai;\n• Load: {load};\n• Capacity: {capacity}.\n\nLatest update on {time_server} (UTC−8, summer UTC−7).'
     bot.send_message(message.chat.id, text, reply_markup=markup_DC)
 
 
 def dc_singapore(message):
     capacity, load, time_server = api_dc.singapore()
-    text = f'🇸🇬 Singapore DCʼ status is OK:\n\n— Load: {load};\n— Capacity: {capacity}.\n\nLatest update on {time_server} (UTC−8, summer UTC−7).'
+    text = f'🇸🇬 Singapore DCʼ status is OK:\n\n• Load: {load};\n• Capacity: {capacity}.\n\nLatest update on {time_server} (UTC−8, summer UTC−7).'
     bot.send_message(message.chat.id, text, reply_markup=markup_DC)
 
 
 def dc_hong_kong(message):
     capacity, load, time_server = api_dc.hong_kong()
-    text = f'🇭🇰 Hong Kong DCʼ status is OK:\n\n— Load: {load};\n— Capacity: {capacity}.\n\nLatest update on {time_server} (UTC−8, summer UTC−7).'
+    text = f'🇭🇰 Hong Kong DCʼ status is OK:\n\n• Load: {load};\n• Capacity: {capacity}.\n\nLatest update on {time_server} (UTC−8, summer UTC−7).'
     bot.send_message(message.chat.id, text, reply_markup=markup_DC)
  
 
@@ -426,7 +426,7 @@ def help(message):
         text = strings.cmdHelp_en
         markup = markup_en
 
-    bot.send_message(message.chat.id, text, parse_mode='html', reply_markup=markup)
+    bot.send_message(message.chat.id, text, parse_mode='html', reply_markup=markup, disable_web_page_preview=True)
 
 
 @bot.message_handler(content_types=['text'])
@@ -444,7 +444,7 @@ def answer(message):
             matchmaking(message)
 
 
-        elif message.text.lower() == 'data centers' or message.text.lower() == 'дата-центры' or message.text.lower() == '/dc':
+        elif message.text.lower() == 'data centers' or message.text.lower() == 'дата-центры (англ.)' or message.text.lower() == '/dc':
             dc(message)
 
 
