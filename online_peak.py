019951ed770fc70f1aebdd487d62c531e68e4d70
abitmoony <31861541+abitmoony@users.noreply.github.com>
2020-12-29 23:23:54 +0000

    Create online_peak.py

---
+++ online_peak.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://steamdb.info/api/RenderAppHover/?appid=730'
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:80.0) Gecko/20100101 Firefox/80.0'}
+
+class peak24h:
+
+    soup = BeautifulSoup(requests.get(url, headers=headers).content, 'html.parser')
+
+    peak = soup.strong.string
+    peak = peak.replace(',', '')
