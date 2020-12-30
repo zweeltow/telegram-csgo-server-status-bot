5ba7112866cb3a0cb782a3b42dcd1fc14dc83f7e
abitmoony <31861541+abitmoony@users.noreply.github.com>
2020-12-30 23:21:01 +0000

    Update online_peak.py

--- online_peak.py
+++ online_peak.py
@@ -1,14 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://steamdb.info/api/RenderAppHover/?appid=730'
+url = 'https://steamdb.info/app/730/graphs/'
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:80.0) Gecko/20100101 Firefox/80.0'}
 
-class Peak24h:
-    def get_peak24h(self):
+class PeakOnline:
+    def get_peak(self):
         soup = BeautifulSoup(requests.get(url, headers=headers).content, 'html.parser')
 
-        peak = soup.strong.string
-        peak = peak.replace(',', '')
+        peak = soup.find_all("strong")
 
-        return peak
+        peak24 = str(peak[1])
+        peak_all = str(peak[2])
+
+        peak24 = peak24.replace('<strong>', '')
+        peak24 = peak24.replace('</strong>', '')
+        peak24 = peak24.replace(',', '')
+
+        peak_all = peak_all.replace('<strong>', '')
+        peak_all = peak_all.replace('</strong>', '')
+        peak_all = peak_all.replace(',', '')
+
+        return peak24, peak_all
