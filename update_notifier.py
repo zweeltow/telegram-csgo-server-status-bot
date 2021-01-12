77a47fc0389d38fdb59e06911325a3caf7dd3cd7
abitmoony <31861541+abitmoony@users.noreply.github.com>
2021-01-12 16:25:05 +0000

    Update update_notifier.py

--- update_notifier.py
+++ update_notifier.py
@@ -61,10 +61,9 @@ def check_for_updates(client):
 
 def send_alert(currentBuild):
     bot = telebot.TeleBot(config.BOT_TOKEN)
-    csgobetachat = -1001280394480 # just for beta works only in t.me/csgobetachat
     text = strings.notiNewBuild_ru.format(currentBuild)
     if not config.TEST_MODE:
-        chat_list = [csgobetachat, config.AQ]
+        chat_list = [config.CSGOBETACHAT, config.AQ]
     else:
         chat_list = [config.OWNER]
     for chatID in chat_list:
