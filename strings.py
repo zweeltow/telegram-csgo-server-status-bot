28483e293986ace1d2b17e000829fb7406fec7c9
abitmoony <31861541+abitmoony@users.noreply.github.com>
2021-01-14 22:13:18 +0000

    Update strings.py

--- strings.py
+++ strings.py
@@ -2,7 +2,7 @@
 # English
 cmdStart_en = '👋🏼 Hey, {}!\nThis bot is designed to check the number of online players and the availability of CS:GO servers.\n\nFor more information type /help.'
 cmdHelp_en = 'ℹ️ This bot is designed by @csgobeta. Source code on <a href="https://github.com/zweeltow/telegram-csgo-server-status-bot">GitHub</a>.\n\n<b>Here are the available commands:</b>\n/status – сheck the status of the servers\n/mm – show the amount of players currently playing\n/devcount - show the amount of devs in beta\n/timer - show the time left until cap reset\n/dc – see the status of a specific data center\n/feedback – leave feedback about the bot\n/help – get this message\n\nNote that this bot also works automatically, no need to add it anywhere. Simply open any of your chats and type ‘<code>@csgobetabot </code>‘ in the message field. Then tap on a result to send.'
-cmdFeedback_en = '💬 Please, tell us about your <b>suggestions</b> or <b>problems</b> that you have encountered using our bot.\n\nUse /cancel to cancel this command.\n\nIf you wish to contact the developer, please PM: @zweel <i>(But I'm bad at English 🙂)</i>.'
+cmdFeedback_en = "💬 Please, tell us about your <b>suggestions</b> or <b>problems</b> that you have encountered using our bot.\n\nUse /cancel to cancel this command.\n\nIf you wish to contact the developer, please PM: @zweel <i>(But I'm bad at English 🙂)</i>."
 # Russian
 cmdStart_ru = '👋🏼 Привет, {}!\nЭтот бот предназначен для проверки количества онлайн игроков и доступности CS:GO серверов.\n\nДля большей информации воспользуйтесь /help.'
 cmdHelp_ru = 'ℹ️ Этот бот разработан каналом @csgobeta. Исходный код на <a href="https://github.com/zweeltow/telegram-csgo-server-status-bot">GitHub</a>.\n\n<b>Доступные команды:</b>\n/status – проверить доступность серверов\n/mm – показать количество онлайн игроков\n/devcount - показать количество разработчиков в бета-версии\n/timer - показать время до сброса ограничений\n/dc – посмотреть состояние определённого дата-центра (на английском языке)\n/feedback – оставить фидбэк про бота\n/help – получить это сообщение\n\nОбратите внимание, что этот бот также работает автоматически, нет необходимости добавлять его куда-либо. Просто откройте любой из Ваших чатов и введите ‘<code>@csgobetabot </code>ʼ в поле сообщения. Затем нажмите на результат для отправки.'
@@ -55,7 +55,7 @@ unknownRequest_ru = '🤷‍♀️ Ничего не найдено, пожал�
 
 '''Text for Wrong API'''
 # English
-wrongAPI_en = '💀 Issues with Valve's API, please try again later.'
+wrongAPI_en = "💀 Issues with Valve's API, please try again later."
 # Russian
 wrongAPI_ru = '💀 Проблемы с API Valve, пожалуйста, попробуйте позже.'
 
