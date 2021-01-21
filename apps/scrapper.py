4a64a8289d02b3048a42b50eee0fec75ca95e0a5
abitmoony <31861541+abitmoony@users.noreply.github.com>
2021-01-21 21:39:57 +0000

    Update scrapper.py

--- apps/scrapper.py
+++ apps/scrapper.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import re
 from bs4 import BeautifulSoup
 from apps import file_manager
 
@@ -8,6 +9,7 @@ import config
 url_st = 'https://store.steampowered.com/stats/'
 url_cs = 'https://blog.counter-strike.net'
 url_ss = 'https://crowbar.steamstat.us/gravity.json'
+url_gv = 'https://raw.githubusercontent.com/SteamDatabase/GameTracking-CSGO/master/csgo/steam.inf'
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:80.0) Gecko/20100101 Firefox/80.0'}
 
 
@@ -74,3 +76,49 @@ class CSGOGameCoordinator:
         except:
             gc_status = 'N/A'
             return gc_status
+
+class GameVersion:
+    def get_gameVer(self):
+        try: 
+            soup = BeautifulSoup(requests.get(url_gv, headers=headers).content, 'html.parser')
+
+            data = soup.get_text()
+
+            f = open(config.GV_CACHE_FILE_PATH, 'r+')
+            f.truncate(0)
+            f.close()
+
+            f = open(config.GV_CACHE_FILE_PATH, 'a')
+            f.write(data)
+            f.close()
+
+            options = {} 
+            f = open(config.GV_CACHE_FILE_PATH)
+            x = f.read()
+            config_entries = re.split('\n|=', x)
+
+            for key, value in zip(config_entries[0::2], config_entries[1::2]):
+                cleaned_key = key.replace("[",'').replace("]",'')
+                options[cleaned_key] = value
+
+            options['ClientVersion'] = int(options['ClientVersion'])
+            options['ServerVersion'] = int(options['ServerVersion'])
+            options['appID'] = int(options['appID'])
+            options['SourceRevision'] = int(options['SourceRevision'])
+
+            line = json.dumps(options, indent=4)
+            fout = open(config.GV_CACHE_FILE_PATH,"w")
+            fout.write(line)
+            fout.close()
+
+            items = file_manager.readJson(config.GV_CACHE_FILE_PATH)
+            client_version = items['ClientVersion']
+            server_version = items['ServerVersion']
+            patch_version = items['PatchVersion']
+            version_date = items['VersionDate']
+            version_time = items['VersionTime']
+
+            return client_version, server_version, patch_version, version_date, version_time
+        except:
+            client_version = server_version = patch_version = version_date = version_time = 'N/A'
+            return client_version, server_version, patch_version, version_date, version_time
